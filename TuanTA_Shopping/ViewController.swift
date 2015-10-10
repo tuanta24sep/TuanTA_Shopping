dfc8a0777169d9cab4de1e7e556062edf6db67a1
Anh Tu·∫•n Tr∆∞∆°ng <tuanta24sep@gmail.com>
2015-10-10 14:28:41 +0700

    Initial Commit

---
+++ TuanTA_Shopping/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TuanTA_Shopping
+//
+//  Created by Anh Tuấn Trương on 10/10/15.
+//  Copyright © 2015 Anh Tuấn Trương. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ TuanTA_ShoppingTests/TuanTA_ShoppingTests.swift
@@ -0,0 +1,36 @@
+//
+//  TuanTA_ShoppingTests.swift
+//  TuanTA_ShoppingTests
+//
+//  Created by Anh Tuấn Trương on 10/10/15.
+//  Copyright © 2015 Anh Tuấn Trương. All rights reserved.
+//
+
+import XCTest
+@testable import TuanTA_Shopping
+
+class TuanTA_ShoppingTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
